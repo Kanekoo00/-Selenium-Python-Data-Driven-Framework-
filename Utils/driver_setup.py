6215b1dc984dc6d9ebf0bf521be742f515991a35
Kanekoo00 <34231977+Kanekoo00@users.noreply.github.com>
2025-11-07 10:31:20 +0700

    Update and rename To Be to driver_setup.py

---
+++ Utils/driver_setup.py
@@ -0,0 +1,13 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
+
+def get_driver():
+    chrome_options = Options()
+    chrome_options.add_argument("--start-maximized")
+    chrome_options.add_argument("--disable-notifications")
+    chrome_options.add_argument("--disable-popup-blocking")
+
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
+    return driver
